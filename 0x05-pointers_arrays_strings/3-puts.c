4e1849ac3ad4118144fd6fc270d6708063767cab
Maryann529 <maryann.pclng@gmail.com>
2022-03-21 04:59:58 -0700

    I do not fear computers.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,13 +6,14 @@
  *
  * Return: void
  */
-void _put(char *str)
+void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (; *str != '\0'; str++)
 	{
-		_putchar(*str[i]);
+		_putchar(*str);
 	}
+
 	_putchar('\n');
 }
