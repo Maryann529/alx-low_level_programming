f0ae86bafa8167e98a9a4d93dd335ecdc18ac591
Maryann529 <maryann.pclng@gmail.com>
2022-03-21 05:05:07 -0700

    I do not fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,12 @@
 
 /**
  * _puts - Prints a string
- * @str: pointer to the string to print
+ * @str: The string to print
  *
  * Return: void
  */
 void _puts(char *str)
 {
-	int i;
-
 	for (; *str != '\0'; str++)
 	{
 		_putchar(*str);
