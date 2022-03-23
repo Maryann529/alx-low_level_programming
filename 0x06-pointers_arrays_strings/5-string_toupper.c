56a33cf366d2deef927d9e758536c860150cd617
Maryann529 <maryann.pclng@gmail.com>
2022-03-23 04:46:24 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - turn every lowercase letter in a string to uppercase.
+ * @str: the stinrg to make all upper
+ *
+ * Return: the string.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	return (str);
+}
