d0d93bf185ad46e03fd87297f1701e5fc0cfee41
Maryann529 <maryann.pclng@gmail.com>
2022-03-23 04:10:39 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncat - concats two strings given bit count of string to add..
+ * @dest: string to add to and return.
+ * @src: string to concat onto dest.
+ * @n: length of string to be added.
+ * Return: dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	
+	for (i = 0; dest[i] != '\0'; ++i)
+		;
+	for (j = 0; j < n && src[j] != '\0'; ++j, ++i)
+		dest[i] = src[j];
+	dest[i] = '\0';
+	return (dest);
+}
