e69cd766cca451ed4b8ea1b5050e89c1c600e7fd
Maryann529 <maryann.pclng@gmail.com>
2022-03-23 04:18:23 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy src into dest and go to n bits.
+ * @dest: where to copy the string and what is returned.
+ * @src: the string to be copied.
+ * @n: the number of places to copy.
+ * Return: dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; ++i)
+		dest[i] = src[i];
+	for ( ; i < n; ++i)
+		dest[i] = '\0';
+	return (dest);
+}
