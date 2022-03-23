d1bd4805aae0ba879e7c9c42d45cb9efa82d76fa
Maryann529 <maryann.pclng@gmail.com>
2022-03-23 12:35:55 -0700

    Expect the best. Prepare for the worst

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,32 @@
 #include "main.h"
 
 /**
- * is_punc - checks for punctuation.
- * @ch: is the char value passed to check if punc.
- * Return: 1 if it is punctuation and 0 if not.
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
  */
-int is_punc(char ch)
+char *cap_string(char *s)
 {
-	if (ch == '.' || ch == ',' || ch == ' ' || ch == '\t' || ch == '\n')
-		return (1);
-	if (ch == '!' || ch == '?' || ch == ';')
-		return (1);
-	if (ch == ')' || ch == '(' || ch == '{' || ch == '}')
-		retrun (1);
-	else
-		return (0);
-}
-/**
- * cap_string - capitalizes first letter of every word.
- * @str: is the input string to be augmented.
- * Return: the changed string..
- */
-char *cap_string(char *str)
-{
-	int i;
+	int 1, j;
 
-	for (i = 0; str[i] != '\0'; ++i)
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
-		else if (is_punc(str[i - 1]) && str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
-	return (str);
 }
