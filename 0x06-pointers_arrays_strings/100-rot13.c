9cb9e15081a2bf27996f7ce089b390dcbbc38c82
Maryann529 <maryann.pclng@gmail.com>
2022-03-23 06:05:06 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encode string  you rot 13 so move 13 spaces.
+ * @str: string to be encoded
+ *
+ * Return: encoded string.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char arr1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char arr2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		for (j = 0; j < 52; ++j)
+		{
+			if (str[i] == arr1[j])
+			{
+				str[i] = arr2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
