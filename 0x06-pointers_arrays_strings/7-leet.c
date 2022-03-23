a60d5271c9e02102e35cb3e5addc241c324a8dc1
Maryann529 <maryann.pclng@gmail.com>
2022-03-23 05:51:58 -0700

    the music not for the elite, but for everybody

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - with only using one "if" statment change into leet speak
+ * @str: string to be turned into leet speak
+ *
+ * Return: augmented string.
+ */
+char *leet(char *str)
+{
+	int i, j;
+	
+	char arr1[] = "aAeEoOtTlL";
+	char arr2[] = "4433007711";
+	
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		for (j = 0; j < 10; ++j)
+		{
+			if (str[i] == arr1[j])
+				str[i] = arr2[j];
+		}
+	}
+	return (str);
+}
