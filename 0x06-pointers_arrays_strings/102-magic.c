75612d1d2aa13feb0567b4251e0f89b835b7ed55
Maryann529 <maryann.pclng@gmail.com>
2022-03-24 06:37:00 -0700

    this was hard

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,10 +11,10 @@ int main(void)
 	/*
 	 * write your line of code here...
 	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
+	 * you are not allowed to use a
+	 * you are not allowed to modify p
+	 * only one statement
+	 * you are not allowed to code anything else than this line of code
 	 */
 	;
 	/* ...so that this prints 98\n */
