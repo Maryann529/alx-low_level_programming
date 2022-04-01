110f67604947fd6d209de055da5f3222083bdf64
Maryann529 <maryann.pclng@gmail.com>
2022-04-01 06:35:52 -0700

    A  program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - adds positive numbers.
+ * @argc: is number of args.
+ * @argv: array of args.
+ * Return: 1 if not number char otherwise 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	sum = 0;
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; ++i)
+	{
+		for (j = 0; argv[i][j] != '\0'; ++j)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+
+	for (i = 1; i < argc; ++i)
+		sum += atoi(argv[i]);
+	printf("%d\n", sum);
+	return (0);
+}
