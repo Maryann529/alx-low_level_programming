28b9789bf33dbc41ae414689dc01627bec312d08
Maryann <maryann.pclng@gmail.com>
2022-04-07 19:35:47 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,69 +1,86 @@
 #include "main.h"
 #include <stdlib.h>
 
+int _atoi(char *s);
+int _strlen(char *s);
 /**
-  * _isdigit - tells if the string consists of digits
-  * @argv: pointer to current item in argument
-  * Return: return 0 if all digits, 1 if not all digits.
-  */
-int _isdigit(char *argv)
+ * main - function with two arguments
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Description: program that multiplies two positive numbers
+ * Return: value
+ */
+int main(int argc, char *argv[])
+{
+	int count, len1, len2, temp1, temp2, *array, *result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+
+	len1 = _strlen(argv[1]);
+	len2 = _strlen(argv[2]);
+	t_len = len1 + len2 - 1;
+
+	array = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (array == NULL)
+		return (NULL);
+
+	len1 -= 1;
+	len2 -= 1;
+	for (count = 1; argv[count] != '\0', count++)
+	{
+		for (; argv[1][len1]; len1--)
+		{
+			temp1 = argv[1][len1 - 1] - '0';;
+		}
+		for (; argv[2][len2]; len2--)
+		{
+			temp2 = argv[2][len2 - 1] - '0';
+		}
+		for (; array[t_len] > 0
+		if ((temp1 * temp2) > 9)
+			array[
+	}
+}
+
+int _atoi(char *s)
+{
+	int i, sign, numb;
+
+	i = 0;
+	sign = 1;
+	numb = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				numb = (s[i] - '0') * sign + numb * 10;
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+	return (numb);
+}
+
+int _strlen(char *s)
 {
 	int i;
 
 	i = 0;
-	while (argv[i])
+	while (*(s + i) != '\0')
 	{
-		if (argv[i] >= '0' && argv[i] <= '9')
-			i++;
-		else
-			return (1);
-	}
-	return (0);
-}
-/**
-  * _atoi - converts a string of ascii digits to the values they represent
-  * @s: pointer to the source string
-  * Return: value of digits
-  */
-int _atoi(char *s)
-{
-	int i, result;
-
-	i = result = 0;
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-		}
 		i++;
 	}
-	return (result);
-}
-/**
-  * main - main function call
-  * @argc: argument count
-  * @argv: 2D array of arguments
-  * Return: return 0 on success, 98 on failure
-  */
-int main(int argc, char *argv[])
-{
-	int i;
-
-	malloc();
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		if (_isdigit(argv[i]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-	return (0);
+	return (i);
 }
