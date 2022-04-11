d80e37413752d5700f775ac567caffc7d5c7337c
Maryann529 <maryann.pclng@gmail.com>
2022-04-11 06:06:27 -0700

    A new type struct dog with the following elements: Name, Age, Owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,22 +9,15 @@
  *
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
 
 #endif
