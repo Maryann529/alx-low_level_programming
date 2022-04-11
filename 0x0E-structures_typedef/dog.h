d69352bdbced820743ffa97071e570bec2a39384
Maryann529 <maryann.pclng@gmail.com>
2022-04-11 06:14:04 -0700

    A new type struct dog with the following elements: Name, Age, Owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,26 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
- * struct - a dog's basic info
- * @name: First memeber
- * @age: Second member
- * @owner: Third member
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
- * Description: Longer description
+ * Description: The attributes of a dog.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
