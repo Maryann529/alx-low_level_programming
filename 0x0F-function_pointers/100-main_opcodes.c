76b671f0156a974f3127e0f484e962597df80728
Maryann529 <maryann.pclng@gmail.com>
2022-04-13 07:00:01 -0700

    #include <stdio.h>

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.hnclude <stdio.h>
+
+#include <stdlib.h>
+
+#include <udis86.h>>
+#include <udis86.h>
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,30 +1,32 @@
 #include "3-calc.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * get_op_func - returns the correct function operation
- * @s: the character to select the function with
+ * get_op_func - ...
+ * @s: ...
  *
- * Return: a function pointer
+ * Return: ...
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div }
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (i < 5)
 	{
-		if (ops[i].op[0] == s[0])
-			break;
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		
 		i++;
 	}
 
-	return (ops[i].f);
+	return (0);
 }
