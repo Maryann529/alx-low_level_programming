54bc76e9277b44256a84d6b1a18114d436d8f473
Maryann529 <maryann.pclng@gmail.com>
2022-04-21 12:49:31 -0700

    Writing a function that prints......

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,12 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - ..
- * Return: Nothing
+ * first - ...
+ *
+ * Return: Nothing.
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
-	printf("I bore my house upon my back! \n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
-
