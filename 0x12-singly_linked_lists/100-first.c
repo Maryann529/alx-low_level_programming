c086385d839db9d0d94aae6c5ab12fe573777acb
Maryann529 <maryann.pclng@gmail.com>
2022-04-21 12:39:30 -0700

    A function that prints 100-first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,11 +3,12 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - ..
+ * Return: Nothing
  */
 void first(void)
 {
-	printf("You.re beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back! \n");
 }
+
