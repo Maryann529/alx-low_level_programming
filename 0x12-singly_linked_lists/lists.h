53bea3e548a59b713d5068c2b8a93b2e6a3d27d3
Maryann529 <maryann.pclng@gmail.com>
2022-04-21 05:26:05 -0700

    The prototypes of all functions and the prototype of the function

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LINKED_LIST
-#define _LINKED_LIST
+#ifndef LIST_H
+#define LIST_H
 
 /**
  * struct list_s - singly linked list
@@ -8,16 +8,15 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for Alx project
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_s;
+} list_t;
 
-int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
@@ -25,3 +24,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
+
