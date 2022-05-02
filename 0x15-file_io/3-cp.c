f778a9123216ad0d964d4b035802ace795182521
Maryann <maryann.pclng@gmail.com>
2022-05-02 18:01:27 +0100

    A program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,14 +23,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
 	{
 		if (fd_w < 0 || write(fd_w, buf, r) != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_r);
-			exit(98);
+			exit(99);
 		}
 	}
 	if (r < 0)
