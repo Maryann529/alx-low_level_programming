0439447e1e6d7505ca777316e2f865b72859a626
Maryann529 <maryann.pclng@gmail.com>
2022-10-03 16:45:31 +0100

    Value in an array of integers

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+#define VALUE_CHECKED(array, index) \
+	printf("Value checked array[%lu] = [%d]\n", index, array[index])
+
+/**
+ * linear_search - search for a value in an array of integers
+ * @array: the array of values
+ * @size: the number of values
+ * @value: the value to locate
+ *
+ * Return: If value is not present in array or array is NULL, return -1.
+ * Otherwise, returh the first index where value is located.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	
+	if (array)
+	{
+		for (i = 0; i < size; ++i)
+		{
+			VALUE_CHECKED(array, i);
+			if (array[i] == value)
+				return (i);
+		}
+	}
+	return (-1);
+}
